1dc548d1a44609cc392086203eeba0af4f378d62
David Novaković <daki.nole@gmail.com>
2026-05-13 12:56:47 +0200

    Added IPvDataService definition

---
+++ Server/Services/PvDataService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+using Common.PvDataContracts;
+
+namespace Server
+{
+    public class PvDataService : IPvDataService
+    {
+        // TODO: Realizovati metode (šta se desi kad klijent pozove PushSample)
+        public void EndSession()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PushSample(PvSample sample)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartSession(PvMeta meta)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
