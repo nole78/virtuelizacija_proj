f61a787dccf3a03eeb1c50bc31c5a1551d75b425
David Novaković <daki.nole@gmail.com>
2026-05-14 14:57:33 +0200

    Implemented base write method and added FileStream and handled its disposing

--- Server/Storage/RejectWriter.cs
+++ Server/Storage/RejectWriter.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Common.PvDataContracts;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +10,20 @@ namespace Server.Storage
 {
     public class RejectWriter : IDisposable
     {
-        // TODO: Implement writing in rejects.csv
+        // TODO: Napraviti tako da radi upis u odgovarajuci folder
         private bool _disposed = false;
-
-        public RejectWriter() { }
+        private FileStream _fileStream;
+        public RejectWriter() 
+        {
+            _fileStream = new FileStream("rejects.csv", FileMode.Append, FileAccess.Write);
+        }
+        public void WriteRow(PvSample sample, string reason)
+        {
+            // TODO: Napraviti tako da se upisuju i ostali podaci, ne samo AcPwrt
+            string line = $"{reason},{sample.Day:o},{sample.Hour},{sample.AcPwrt}\n";
+            byte[] lineInBytes = new UTF8Encoding().GetBytes(line + Environment.NewLine);
+            _fileStream.Write(lineInBytes, 0, lineInBytes.Length);
+        }
         ~RejectWriter()
         {
             Dispose(false);
@@ -28,6 +40,9 @@ namespace Server.Storage
             if(disposing)
             {
                 // Dispose managed resources here
+                _fileStream.Flush();
+                _fileStream.Dispose();
+                _fileStream = null;
             }
             // Dispose unmanaged resources here
             _disposed = true;
--- Server/Storage/SessionWriter.cs
+++ Server/Storage/SessionWriter.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Common.PvDataContracts;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +10,19 @@ namespace Server.Storage
 {
     public class SessionWriter : IDisposable
     {
-        // TODO: Implementirati
+        // TODO: Napraviti tako da radi upis u odgovarajuci folder
         private bool _disposed = false;
-        SessionWriter() { }
+        private FileStream _fileStream;
+        SessionWriter() {
+            _fileStream = new FileStream("sessions.csv", FileMode.Append, FileAccess.Write);
+        }
+        public void WriteRow(PvSample sample)
+        {
+            // TODO: Napraviti tako da se upisuju i ostali podaci, ne samo AcPwrt
+            string line = $"{sample.Day:o},{sample.Hour},{sample.AcPwrt}\n";
+            byte[] lineInBytes = new UTF8Encoding().GetBytes(line + Environment.NewLine);
+            _fileStream.Write(lineInBytes, 0, lineInBytes.Length);
+        }
         ~SessionWriter()
         {
             Dispose(false);
@@ -26,6 +38,9 @@ namespace Server.Storage
             if (disposing)
             {
                 // Oslobodi managed resurse
+                _fileStream.Flush();
+                _fileStream.Dispose();
+                _fileStream = null;
             }
             // Oslobodi unmanaged resurse
             _disposed = true;
