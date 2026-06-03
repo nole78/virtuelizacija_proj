d4f84819ec4122103891c4f08f48f9d388c80da6
David Novaković <daki.nole@gmail.com>
2026-06-03 12:34:41 +0200

    Implemented ConsoleLogger subscriber

---
+++ Server/Events/ConsoleLogger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Events
+{
+    public class ConsoleLogger
+    {
+        public void Subscribe(TransferEventHub hub)
+        {
+            hub.OnTransferStarted += TransferStarted;
+            hub.OnSampleReceived += SampleReceived;
+            hub.OnTransferCompleted += TransferCompleted;
+            hub.OnWarningRaised += WarningRaised;
+        }
+
+        private void TransferStarted(object sender, TransferEventArgs e)
+        {
+            Console.WriteLine("[INFO] " + e.Message);
+        }
+        private void SampleReceived(object sender, TransferEventArgs e)
+        {
+            Console.WriteLine("[INFO]" + e.Message);
+        }
+        private void TransferCompleted(object sender, TransferEventArgs e)
+        {
+            Console.WriteLine("[INFO] " + e.Message);
+        }
+        private void WarningRaised(object sender, TransferEventArgs e)
+        {
+            Console.WriteLine($"[WARNING] [${e.WarningType}] ${e.Message}");
+        }
+    }
+}
