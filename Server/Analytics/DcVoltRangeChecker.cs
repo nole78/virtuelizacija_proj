88e47b4251c8830f67798b3cd9c548929ee41004
anja-sreckovic <anja.sreckovic24@gmail.com>
2026-06-11 14:26:34 +0200

    Add DcVoltRangeChecker

--- Server/Analytics/DcVoltRangeChecker.cs
+++ Server/Analytics/DcVoltRangeChecker.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Server.Config;
+using Server.Events;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,34 @@ namespace Server.Analytics
 {
     public class DcVoltRangeChecker
     {
-        // TODO: Implementirati (Zad 10)
+        private double _dcVoltMin = 0;
+        private double _dcVoltMax = 0;
+        private readonly TransferEventHub _transferEventHub;
+        
+        public DcVoltRangeChecker(TransferEventHub transferEventHub) 
+        {
+            _dcVoltMin = ThresholdConfig.DcVoltMin;
+            _dcVoltMax = ThresholdConfig.DcVoltMax;
+            _transferEventHub = transferEventHub;
+        }
+        public void RunCheck(double dcVolt)
+        {
+            /*DC napon van opsega: proveravati DCVOLT po svakom redu; ako vrednost padne ispod
+            DcVoltMin ili poraste iznad DcVoltMax (iz konfiguracije) → podići
+            DcVoltOutOfRangeWarning.*/
+            //Ovo je razdvojeno samo kako bi poruke bile "tacnije"
+            if(dcVolt < _dcVoltMin)
+            {
+                _transferEventHub.RaiseWarning(new TransferEventArgs(
+                    WarningType.DcVoltOutOfRange,
+                    $"DcVolt value: {Math.Round(dcVolt, 4)} | DcVoltMin: {Math.Round(_dcVoltMin, 2)}"));
+            }
+            else if (dcVolt > _dcVoltMax)
+            {
+                _transferEventHub.RaiseWarning(new TransferEventArgs(
+                    WarningType.DcVoltOutOfRange,
+                    $"DcVolt value: {Math.Round(dcVolt, 4)} | DcVoltMax: {Math.Round(_dcVoltMax, 2)}"));
+            }
+        }
     }
 }
--- Server/Services/PvDataService.cs
+++ Server/Services/PvDataService.cs
@@ -29,6 +29,7 @@ namespace Server
         private int _receivedSamplesCount = 0;
         private double _procenat = 0;
         private CurrentSpikeDetector _spikeDetector;
+        private DcVoltRangeChecker _dcVoltChecker;
 
         private DateTime _lastActivity;
         private Timer _sessionWatchdog;
@@ -80,6 +81,7 @@ namespace Server
 
                 // PROVERE
                 _spikeDetector.RunCheck((double)sample.AcCur1);
+                _dcVoltChecker.RunCheck((double)sample.DcVolt);
             }
         }
 
@@ -113,6 +115,7 @@ namespace Server
 
             _sampleValidator = new SampleValidator();
             _spikeDetector = new CurrentSpikeDetector(_eventHub);
+            _dcVoltChecker = new DcVoltRangeChecker(_eventHub);
 
             _lastActivity = DateTime.Now;
 
